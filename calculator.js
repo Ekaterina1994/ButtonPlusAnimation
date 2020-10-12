210d23474d0acba68b7ffa815ca6c10b666c79cf
Ekaterina1994 <katishteno@mail.ru>
2020-10-12 19:44:51 +0300

    add files

---
+++ calculator.js
@@ -0,0 +1,26 @@
+  function random() {
+    document.form.textView.value = Math.floor(Math.random() * 100);
+  }
+  function insert(num) {
+    document.form.textView.value = document.form.textView.value + num;
+  }
+  function clean() {
+    document.form.textView.value = "";
+  }
+  function back() {
+    let exp = document.form.textView.value;
+    document.form.textView.value = exp.substring(0,exp.length-1);
+  }
+  function equal() {
+    let exp = document.form.textView.value;
+    if(exp) {
+      document.form.textView.value = eval(exp);
+    }
+  }
+  function go() {
+    const img = document.getElementById('img');
+    const btn = document.getElementById('btn');
+    btn.addEventListener('click', () => {
+      img.style.animationName = "slidein";
+    })
+  }
